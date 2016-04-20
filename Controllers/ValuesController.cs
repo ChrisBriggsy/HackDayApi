aaf4763242d627da1cfb3470beeb5d3017278a80
Chris Briggs <coldfire_vortex@hotmail.com>
2016-04-20 23:37:30 +1000

    Initial Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace HackDayAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class WeatherController : ApiController
+    {
+        private static List<WeatherData> _weatherData = new List<WeatherData>();
+        
+        public List<WeatherData> Get()
+        {
+            return _weatherData;
+        }
+
+        public void Post([FromBody]WeatherData value)
+        {
+            _weatherData.Add(value);
+        }
+
+        public void Delete()
+        {
+            _weatherData = new List<WeatherData>();
+        }
+    }
+
+    public class WeatherData
+    {
+        public string DeviceID { get; set; }
+        public float Humidity { get; set; }
+        public float Temperature { get; set; }
+    }
+}
